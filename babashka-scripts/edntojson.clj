01f19a88e5b131da026a56bfd9a3dd9930b0f3e8
shivekkhurana <khuranashivek@outlook.com>
2020-04-10 07:43:50 +0530

    Brought some sans-serif fonts, parallelised font->svg scripts, move babashka scripts here

---
+++ babashka-scripts/edntojson.clj
@@ -0,0 +1,31 @@
+;; Copies all edn files in a dir to their json counterpart
+;; The edn files in sub directory are not touched
+
+(def args *command-line-args*)
+
+(when-not (= 1 (count args))
+  (println "Invalid arguments")
+  (println "Usage: bb edntojson.clj <source-dir>")
+  (System/exit 1))
+
+(defn ls-raw [dir]
+  (-> dir
+      io/file
+      file-seq))
+
+(defn ls-edn-files [dir]
+  (->> dir
+       ls-raw
+       (filter #(.isFile %))
+       (filter #(re-matches #".*.edn" (.getName %)))
+       (map #(.getPath %))))
+
+(defn edn->json [path]
+  (let [json-path (str/replace path #".edn" ".json")]
+    (println :> path :--> json-path )
+    (spit json-path (json/encode (slurp path)))))
+
+(let [[source-dir] args
+      ednz (ls-edn-files source-dir)]
+  (doall
+   (pmap edn->json ednz)))
---
+++ babashka-scripts/fonttosvg.clj
@@ -0,0 +1,75 @@
+;; For a given directory of raw font files (ttf or otf)
+;; Generate folders titled eg-font-name
+;; Convert each gryph in EGFontName.ttf to an svg in eg-font-name/a_EGFontname.svg
+;; And finally create a meta file that serves as an index
+
+(def args *command-line-args*)
+
+(when-not (= 3 (count args))
+  (println "Invalid arguments")
+  (println "Usage:" "bb fonttosvg.clj <serif-source-dir> <sans-serif-source-dir> <sink-dir>")
+  (println "Note: The paths should be absoloute and not have trailing slashes")
+  (System/exit 1))
+
+(defn bash [command]
+  (shell/sh "bash" "-c"
+            command))
+
+(defn font->svg [font-path font-file-name source-dir]
+  (when (not= font-file-name ".DS_Store")
+    (println "Processing" font-file-name "in" font-path)
+    (let [command (str "cd " font-path
+                       "; fontforge -lang=ff -c 'Open(\"" source-dir "/" font-file-name
+                       "\"); SelectWorthOutputting(); foreach Export(\"svg\"); endloop;'" )]
+      ;;(println command)
+      (bash command)
+      )))
+
+(defn list-files [dir]
+  (->> dir
+       io/file
+       file-seq
+       (filter #(.isFile %))))
+
+(defn process-font-file [src-dir sink-dir index-atom family f]
+  (let [raw-name (.getName f)
+        [_ basename] (re-matches #"(.*).[t|o]tf" raw-name)
+        normalized-name (when basename
+                          (-> basename
+                              str/lower-case
+                              (str/replace #"_" "-")))
+        font-path (str sink-dir "/" normalized-name)]
+
+    (.mkdir (java.io.File. font-path))
+    ;; if path doesn't exist, vectors will be spit in dir of execution
+    (wait/wait-for-path font-path)
+
+    (swap! index-atom conj {:family family
+                            :normalized-name normalized-name
+                            :base-name basename
+                            :raw raw-name})
+    (font->svg (str sink-dir "/" normalized-name)
+               raw-name src-dir)
+    ))
+
+
+(let [index-atom (atom [])
+      [serif-dir sans-serif-dir sink-dir] args]
+
+  (println "Generating svgs...")
+  (doall (pmap
+          (partial process-font-file serif-dir sink-dir index-atom :serif)
+          (list-files serif-dir)))
+
+  (doall (pmap
+          (partial process-font-file sans-serif-dir sink-dir index-atom :sans-serif)
+          (list-files sans-serif-dir)))
+
+  (println "Writing meta...")
+  (spit (str sink-dir "/meta.edn")
+        (prn-str @index-atom))
+
+  (spit (str sink-dir "/meta.json")
+        (prn-str (json/encode @index-atom)))
+  )
+
